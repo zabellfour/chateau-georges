32ad4fb48c59d5b2675f1fc6983d13d77567652a
zabellfour <zabell2814@gmail.com>
2016-10-28 00:15:26 +0300

    added header and footer

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,326 @@
-import HP from './modules/helpers';
+jQuery(function() {
+    initTabs();
+});
 
-(function ($) {
 
-  // When DOM is ready
-  $(function () {
-    HP.random(10, 20);
-  });
+$(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+        items:1
+    });
+});
 
+function initMobileNav() {
+    jQuery('body').mobileNav({
+        hideOnClickOutside: true,
+        menuActiveClass: 'nav-active',
+        menuOpener: '.nav-opener',
+        menuDrop: '.nav-drop'
+    });
+}
+
+
+function initTabs() {
+    jQuery('ul.tab-list').tabset({
+        tabLinks: 'a',
+        attrib: 'data-tab',
+        defaultTab: false
+    });
+}
+
+
+$(function() {
+    $('.item').matchHeight({
+        byRow: true,
+        property: 'height',
+        target: null,
+        remove: false
+    });
+});
+
+/*
+ * jQuery Tabs plugin
+ */
+
+;
+(function($, $win) {
+    'use strict';
+
+    function Tabset($holder, options) {
+        this.$holder = $holder;
+        this.options = options;
+
+        this.init();
+    }
+
+    Tabset.prototype = {
+        init: function() {
+            this.$tabLinks = this.$holder.find(this.options.tabLinks);
+
+            this.setStartActiveIndex();
+            this.setActiveTab();
+
+            if (this.options.autoHeight) {
+                this.$tabHolder = $(this.$tabLinks.eq(0).attr(this.options.attrib)).parent();
+            }
+        },
+
+        setStartActiveIndex: function() {
+            var $classTargets = this.getClassTarget(this.$tabLinks);
+            var $activeLink = $classTargets.filter('.' + this.options.activeClass);
+            var $hashLink = this.$tabLinks.filter('[' + this.options.attrib + '="' + location.hash + '"]');
+            var activeIndex;
+
+            if (this.options.checkHash && $hashLink.length) {
+                $activeLink = $hashLink;
+            }
+
+            activeIndex = $classTargets.index($activeLink);
+
+            this.activeTabIndex = this.prevTabIndex = (activeIndex === -1 ? (this.options.defaultTab ? 0 : null) : activeIndex);
+        },
+
+        setActiveTab: function() {
+            var self = this;
+
+            this.$tabLinks.each(function(i, link) {
+                var $link = $(link);
+                var $classTarget = self.getClassTarget($link);
+                var $tab = $($link.attr(self.options.attrib));
+
+                if (i !== self.activeTabIndex) {
+                    $classTarget.removeClass(self.options.activeClass);
+                    $tab.addClass(self.options.tabHiddenClass).removeClass(self.options.activeClass);
+                } else {
+                    $classTarget.addClass(self.options.activeClass);
+                    $tab.removeClass(self.options.tabHiddenClass).addClass(self.options.activeClass);
+                }
+
+                self.attachTabLink($link, i);
+            });
+        },
+
+        attachTabLink: function($link, i) {
+            var self = this;
+
+            $link.on(this.options.event + '.tabset', function(e) {
+                e.preventDefault();
+
+                if (self.activeTabIndex === self.prevTabIndex && self.activeTabIndex !== i) {
+                    self.activeTabIndex = i;
+                    self.switchTabs();
+                }
+            });
+        },
+
+        resizeHolder: function(height) {
+            var self = this;
+
+            if (height) {
+                this.$tabHolder.height(height);
+                setTimeout(function() {
+                    self.$tabHolder.addClass('transition');
+                }, 10);
+            } else {
+                self.$tabHolder.removeClass('transition').height('');
+            }
+        },
+
+        switchTabs: function() {
+            var self = this;
+
+            var $prevLink = this.$tabLinks.eq(this.prevTabIndex);
+            var $nextLink = this.$tabLinks.eq(this.activeTabIndex);
+
+            var $prevTab = this.getTab($prevLink);
+            var $nextTab = this.getTab($nextLink);
+
+            $prevTab.removeClass(this.options.activeClass);
+
+            if (self.haveTabHolder()) {
+                this.resizeHolder($prevTab.outerHeight());
+            }
+
+            setTimeout(function() {
+                self.getClassTarget($prevLink).removeClass(self.options.activeClass);
+
+                $prevTab.addClass(self.options.tabHiddenClass);
+                $nextTab.removeClass(self.options.tabHiddenClass).addClass(self.options.activeClass);
+
+                self.getClassTarget($nextLink).addClass(self.options.activeClass);
+
+                if (self.haveTabHolder()) {
+                    self.resizeHolder($nextTab.outerHeight());
+
+                    setTimeout(function() {
+                        self.resizeHolder();
+                        self.prevTabIndex = self.activeTabIndex;
+                    }, self.options.animSpeed);
+                } else {
+                    self.prevTabIndex = self.activeTabIndex;
+                }
+            }, this.options.autoHeight ? this.options.animSpeed : 1);
+        },
+
+        getClassTarget: function($link) {
+            return this.options.addToParent ? $link.parent() : $link;
+        },
+
+        getActiveTab: function() {
+            return this.getTab(this.$tabLinks.eq(this.activeTabIndex));
+        },
+
+        getTab: function($link) {
+            return $($link.attr(this.options.attrib));
+        },
+
+        haveTabHolder: function() {
+            return this.$tabHolder && this.$tabHolder.length;
+        },
+
+        destroy: function() {
+            var self = this;
+
+            this.$tabLinks.off('.tabset').each(function() {
+                var $link = $(this);
+
+                self.getClassTarget($link).removeClass(self.options.activeClass);
+                $($link.attr(self.options.attrib)).removeClass(self.options.activeClass + ' ' + self.options.tabHiddenClass);
+            });
+
+            this.$holder.removeData('Tabset');
+        }
+    };
+
+    $.fn.tabset = function(options) {
+        options = $.extend({
+            activeClass: 'active',
+            addToParent: false,
+            autoHeight: false,
+            checkHash: false,
+            defaultTab: true,
+            animSpeed: 500,
+            tabLinks: 'a',
+            attrib: 'href',
+            event: 'click',
+            tabHiddenClass: 'js-tab-hidden'
+        }, options);
+        options.autoHeight = options.autoHeight && $.support.opacity;
+
+        return this.each(function() {
+            var $holder = $(this);
+
+            if (!$holder.data('Tabset')) {
+                $holder.data('Tabset', new Tabset($holder, options));
+            }
+        });
+    };
+}(jQuery, jQuery(window)));
+
+
+
+/*
+ * Simple Mobile Navigation
+ */
+;
+(function($) {
+    function MobileNav(options) {
+        this.options = $.extend({
+            container: null,
+            hideOnClickOutside: false,
+            menuActiveClass: 'nav-active',
+            menuOpener: '.nav-opener',
+            menuDrop: '.nav-drop',
+            toggleEvent: 'click',
+            outsideClickEvent: 'click touchstart pointerdown MSPointerDown'
+        }, options);
+        this.initStructure();
+        this.attachEvents();
+    }
+    MobileNav.prototype = {
+        initStructure: function() {
+            this.page = $('html');
+            this.container = $(this.options.container);
+            this.opener = this.container.find(this.options.menuOpener);
+            this.drop = this.container.find(this.options.menuDrop);
+        },
+        attachEvents: function() {
+            var self = this;
+
+            if (activateResizeHandler) {
+                activateResizeHandler();
+                activateResizeHandler = null;
+            }
+
+            this.outsideClickHandler = function(e) {
+                if (self.isOpened()) {
+                    var target = $(e.target);
+                    if (!target.closest(self.opener).length && !target.closest(self.drop).length) {
+                        self.hide();
+                    }
+                }
+            };
+
+            this.openerClickHandler = function(e) {
+                e.preventDefault();
+                self.toggle();
+            };
+
+            this.opener.on(this.options.toggleEvent, this.openerClickHandler);
+        },
+        isOpened: function() {
+            return this.container.hasClass(this.options.menuActiveClass);
+        },
+        show: function() {
+            this.container.addClass(this.options.menuActiveClass);
+            if (this.options.hideOnClickOutside) {
+                this.page.on(this.options.outsideClickEvent, this.outsideClickHandler);
+            }
+        },
+        hide: function() {
+            this.container.removeClass(this.options.menuActiveClass);
+            if (this.options.hideOnClickOutside) {
+                this.page.off(this.options.outsideClickEvent, this.outsideClickHandler);
+            }
+        },
+        toggle: function() {
+            if (this.isOpened()) {
+                this.hide();
+            } else {
+                this.show();
+            }
+        },
+        destroy: function() {
+            this.container.removeClass(this.options.menuActiveClass);
+            this.opener.off(this.options.toggleEvent, this.clickHandler);
+            this.page.off(this.options.outsideClickEvent, this.outsideClickHandler);
+        }
+    };
+
+    var activateResizeHandler = function() {
+        var win = $(window),
+            doc = $('html'),
+            resizeClass = 'resize-active',
+            flag, timer;
+        var removeClassHandler = function() {
+            flag = false;
+            doc.removeClass(resizeClass);
+        };
+        var resizeHandler = function() {
+            if (!flag) {
+                flag = true;
+                doc.addClass(resizeClass);
+            }
+            clearTimeout(timer);
+            timer = setTimeout(removeClassHandler, 500);
+        };
+        win.on('resize orientationchange', resizeHandler);
+    };
+
+    $.fn.mobileNav = function(options) {
+        return this.each(function() {
+            var params = $.extend({}, options, { container: this }),
+                instance = new MobileNav(params);
+            $.data(this, 'MobileNav', instance);
+        });
+    };
 }(jQuery));
--- src/vendor_entries/vendor.js
+++ src/vendor_entries/vendor.js
@@ -1,3 +1,6 @@
 module.exports = [
-  './bower/jquery/dist/jquery.js'
+    './bower/jquery/dist/jquery.js',
+    './bower/bootstrap-sass/assets/javascripts/bootstrap.min.js',
+    './bower/owl.carousel/dist/owl.carousel.min.js',
+    './bower/matchHeight/dist/jquery.matchHeight-min.js'
 ];
